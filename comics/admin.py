3b5d6bb11384d0e92fa219f785b68343834fdaa4
Santoso Wijaya <santa.ant@me.com>
2010-06-01 00:34:37 -0700

    Add an 'external' field to embed a media.
    Make this new field, and 'strip' FileField, optional (blank=True).

--- comics/admin.py
+++ comics/admin.py
@@ -2,7 +2,7 @@ from engineered.comics.models import Strip
 from django.contrib import admin
 
 class StripAdmin(admin.ModelAdmin):
-    fields = ['sub_date', 'title', 'alt_text', 'strip']
+    fields = ['sub_date', 'title', 'alt_text', 'strip', 'external']
     list_filter = ['sub_date']
     search_fields = ['title']
     date_hierarchy = 'sub_date'
